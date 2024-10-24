f293c1372b45f774ec4170d151db30e173edf2e2
MD ABDULLAH <110069678+CoderBDK@users.noreply.github.com>
2024-10-24 06:56:27 +0600

    Create bisection_method.c

---
+++ bisection_method.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double x) {
+    return x*x - 4;
+}
+void bisection(double a, double b, double tol) {
+    if (f(a) * f(b) >= 0) {
+        printf("Bisection method fails.\n");
+        return;
+    }
+    double c;
+    while ((b - a) >= tol) {
+        c = (a + b) / 2;
+        if (f(c) == 0.0) break;
+        else if (f(c) * f(a) < 0) b = c;
+        else a = c;
+    }
+    printf("Root: %lf\n", c);
+}
